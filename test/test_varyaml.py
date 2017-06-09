eab33dc51a4597525e786fdaeb89b8a51e516197
Abe Winter <abe-winter@users.noreply.github.com>
2017-06-09 17:16:53 -0400

    passing

--- test/test_varyaml.py
+++ test/test_varyaml.py
@@ -2,28 +2,45 @@ import varyaml, pytest, itertools, os, yaml
 
 os.environ['VY_KEY'] = 'hello'
 
+def load(raw):
+    "helper to dump & parse"
+    return varyaml.load(yaml.dump(raw))
+
 def test_dict():
-    ret = varyaml.load(yaml.dump({'a':'$VY_KEY', 'b':1, 'c':'ok'}))
+    ret = load({'a':'$VY_KEY', 'b':1, 'c':'ok'})
     assert ret['a'] == os.environ['VY_KEY']
     assert ret['b'] == 1
     assert ret['c'] == 'ok'
 
 def test_list():
-    ret = varyaml.load(yaml.dump(['$VY_KEY', 1, 'ok']))
+    ret = load(['$VY_KEY', 1, 'ok'])
     assert ret[0] == os.environ['VY_KEY']
     assert ret[1] == 1
     assert ret[2] == 'ok'
 
-def test_omit_dict():
-    raise NotImplementedError
-
-def test_omit_list():
-    raise NotImplementedError
+def test_dict_default():
+    assert 'X1' not in os.environ
+    assert 'X3' not in os.environ
+    os.environ['X0'] = '50'
+    os.environ['X2'] = '60'
+    ret = load({
+        'omittable':'$X0', 'omitted':'$X1', 'present':'$X2', 'default':'$X3',
+        'varyaml':{'X0':'__omit__', 'X1':'__omit__', 'X2':1, 'X3':1},
+    })
+    assert ret['omittable'] == '50'
+    assert 'omitted' not in ret
+    assert ret['present'] == '60'
+    assert ret['default'] == 1
 
 def test_missing_error():
     "also test path here"
-    raise NotImplementedError
+    assert 'MISSING' not in os.environ
+    with pytest.raises(KeyError) as exc:
+        load([{'missing':'$MISSING'}])
+    assert exc.value.args == ('MISSING', '/0/missing')
 
-@pytest.mark.parametrize('t1,t2', itertools.product((list, dict), (list, dict)))
-def test_nest_cases(t1, t2):
-    raise NotImplementedError
+def test_nest_cases():
+    assert load({'a':{'b':'$VY_KEY'}})['a']['b'] == 'hello'
+    assert load({'a':['$VY_KEY']})['a'][0] == 'hello'
+    assert load([['$VY_KEY']])[0][0] == 'hello'
+    assert load([{'b':'$VY_KEY'}])[0]['b'] == 'hello'
--- varyaml.py
+++ varyaml.py
@@ -6,39 +6,48 @@ def substitute(var, defaults, path):
     if var in os.environ:
         # todo: do we want to support redirection here? see top-level README.md
         return os.environ[var]
-    elif var in defaults:
-        return defaults[var] == '__omit__'
+    elif defaults and var in defaults:
+        return defaults[var]
     else:
-        raise KeyError('%s not found (at %s)' % (var, path))
+        raise KeyError(var, path)
 
 def process(item, defaults=None, path=''):
     "recursively processes containers. mutates input."
     iter_ = None
     if isinstance(item, dict):
-        iter_ = dict.items()
+        iter_ = item.items()
     elif isinstance(item, list):
         iter_ = enumerate(item)
     else:
-        return
+        return item
 
     any_omit = False
+    pops = []
     for index, val in iter_:
         if isinstance(val, (dict, list)):
             process(val, defaults, '%s/%s' % (path, index))
         elif isinstance(val, str) and val.startswith('$'):
-            newval = substitute(val[1:], defaults, path+k+'/')
+            newval = substitute(val[1:], defaults, '%s/%s' % (path, index))
             if newval == '__omit__':
                 if isinstance(item, list):
                     item[index] = Omit
                     any_omit = True
                 elif isinstance(item, dict):
-                    item.pop(index)
+                    pops.append(index)
+            else:
+                item[index] = newval
 
+    if pops:
+        assert isinstance(item, dict)
+        for k in pops:
+            item.pop(k)
     if any_omit:
         assert isinstance(item, list)
         item[:] = (x for x in item if x is not Omit)
 
+    return item
+
 def load(*args, **kwargs):
     data = yaml.load(*args, **kwargs)
-    defaults = d.get('varyaml') if isinstance(d, dict) else None
+    defaults = data.get('varyaml') if isinstance(data, dict) else None
     return process(data, defaults)
